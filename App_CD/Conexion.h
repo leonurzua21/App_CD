4e65495c9bec977cf4cab712bee5158d5c58b2dc
Leonardo Coronel <leonardo.coronel04@outlook.com>
2025-03-25 11:25:27 -0600

    Conexion remota Lista

--- App_CD/Conexion.h
+++ App_CD/Conexion.h
@@ -3,25 +3,35 @@
 using namespace System;
 using namespace System::Configuration;
 using namespace System::Data;
-using namespace System::Data::SqlClient;
+using namespace MySql::Data;
+using namespace MySqlClient;
 
 ref class conexion
 {
-    SqlConnection^ conecta;
-    SqlConnectionStringBuilder^ st;
+    MySqlConnection^ conecta;
+    MySqlConnectionStringBuilder^ st;
 public:
     conexion();
 
 protected:
     void Conectar()
 	{
+<<<<<<< HEAD
 		st = gcnew SqlConnectionStringBuilder();
 		st->DataSource = "192.168.1.73"; //Base de datos remota
 		st->InitialCatalog = "Casa_dom";
 		st->UserID = "Test";
 		st->Password = "pbadatab2024!";       
+=======
+        st = gcnew MySqlConnectionStringBuilder();
+        st->Server = "bvyfz6mfmtdrhrb6tfow-mysql.services.clever-cloud.com";
+        st->Database = "bvyfz6mfmtdrhrb6tfow";
+        st->UserID = "u1rkvy1adcgozdfu";
+        st->Password = "nL9p8U0gipad3TVVINH1";
+        st->SslMode = MySqlSslMode::Required;
+>>>>>>> c867218 (Conexion remota Lista)
 
-        conecta = gcnew SqlConnection(Convert::ToString(st));
+        conecta = gcnew MySqlConnection(Convert::ToString(st));
     }
 public:
     void Insertar(String^ nombre, String^ apellido, String^ UserName, String^ UserPass)
@@ -31,15 +41,28 @@ public:
             Conectar();
         }
 
-        String^ enviardatos = "Insert into Usuario values (@nombre,@apellido,@usuario,@pass)";
-        SqlCommand^ ejecutar = gcnew SqlCommand(enviardatos, conecta);
+        String^ enviardatos = "INSERT INTO usuario values (@nombre,@apellido,@usuario,@pass)";
+        MySqlCommand^ ejecutar = gcnew MySqlCommand(enviardatos, conecta);
         ejecutar->Parameters->AddWithValue("@nombre",nombre);
         ejecutar->Parameters->AddWithValue("@apellido",apellido);
         ejecutar->Parameters->AddWithValue("@usuario",UserName);
         ejecutar->Parameters->AddWithValue("@pass",UserPass);
 
+<<<<<<< HEAD
         conecta->Open();
         ejecutar->ExecuteNonQuery();
         conecta->Close();
+=======
+        try {
+            conecta->Open();
+            ejecutar->ExecuteNonQuery();
+        }
+        catch (Exception^ ex) {
+            Console::WriteLine("Error: " + ex->Message);
+        }
+        finally {
+            conecta->Close();
+        }
+>>>>>>> c867218 (Conexion remota Lista)
     }
 };
--- App_CD/Consultar.h
+++ App_CD/Consultar.h
@@ -1,35 +1,44 @@
 #pragma once
 using namespace System;
 using namespace System::Configuration;
-using namespace System::Data;
-using namespace System::Data::SqlClient;
+using namespace MySql::Data;
+using namespace MySqlClient;
 
 ref class Consultar
 {
-	static SqlConnection^ connect;
-	static SqlConnectionStringBuilder^ ts;
+	static MySqlConnection^ connect;
+	static MySqlConnectionStringBuilder^ ts;
 public:
 	Consultar();
 protected:
 	static void 
 		ObtenerDatos()
 	{
+<<<<<<< HEAD
 		ts = gcnew SqlConnectionStringBuilder();
 		ts->DataSource = "DESKTOP-GJGNJ95\\SQLEXPRESS";
 		ts->InitialCatalog = "Casa_dom";
 		ts->IntegratedSecurity = "True";
+=======
+		ts = gcnew MySqlConnectionStringBuilder();
+		ts->Server = "bvyfz6mfmtdrhrb6tfow-mysql.services.clever-cloud.com";
+		ts->Database = "bvyfz6mfmtdrhrb6tfow";
+		ts->UserID = "u1rkvy1adcgozdfu";
+		ts->Password = "nL9p8U0gipad3TVVINH1";
+		ts->SslMode = MySqlSslMode::Required;
+>>>>>>> c867218 (Conexion remota Lista)
 
-		connect = gcnew SqlConnection(Convert::ToString(ts));
+		connect = gcnew MySqlConnection(Convert::ToString(ts));
 	}
 public:
 	static bool UserExists(String^ userName, String^ userPass)  //Ingresa con nombre de usuario
 	{ //Abre una conexión a la BD y verifica si el usuario existe
 		ObtenerDatos();
 		
-        String^ query = "SELECT COUNT(*) FROM Usuario WHERE usuario = '" + userName + "' AND pass = '" + userPass + "'"; //Consulta el conteo de los usuarios registrados en la tabla Usuario
-		SqlCommand^ cmd = gcnew SqlCommand(query, connect);
+        String^ query = "SELECT COUNT(*) FROM usuario WHERE usuario = '" + userName + "' AND pass = '" + userPass + "'"; //Consulta el conteo de los usuarios registrados en la tabla Usuario
+		MySqlCommand^ cmd = gcnew MySqlCommand(query, connect);
 		connect->Open();
-		int count = (int)cmd->ExecuteScalar();
+		int count = Convert::ToInt32(cmd->ExecuteScalar());
 		connect->Close();
 		if (count > 0)
 			return true;
