410c65516644a4734b70589062044db1e82b4c1c
Leonardo Coronel <leonardo.coronel04@outlook.com>
2025-03-25 16:28:13 -0600

    conexion a base de datos remota

--- App_CD/Consultar.h
+++ App_CD/Consultar.h
@@ -39,7 +39,7 @@ public:
 			return false;
 	}
 public:
-	static bool UsuarioExists(String^ userName)  //Ingresa con nombre de usuario, metodo utilizado en la clase Registro.
+	static bool UsuarioExists(String^ userName)  //Ingresa con nombre de usuario, metodo utilizado en la clase Registro.h
 	{ //Abre una conexión a la BD y verifica si el usuario existe
 		ObtenerDatos();
 
