511ad13cf6af12f23c202b407dada73568a5896e
leonurzua21 <95547022+leonurzua21@users.noreply.github.com>
2025-03-25 16:03:26 -0600

    Add files via upload

--- App_CD/Conexion.h
+++ App_CD/Conexion.h
@@ -34,24 +34,12 @@ public:
             Conectar();
         }
 
-<<<<<<< HEAD
         String^ enviardatos = "INSERT INTO usuario (nombre,apellido,usuario,pass) values (@nombre,@apellido,@usuario,@pass)";
         MySqlCommand^ ejecutar = gcnew MySqlCommand(enviardatos, conecta);
         ejecutar->Parameters->AddWithValue("@nombre", nombre);
         ejecutar->Parameters->AddWithValue("@apellido", apellido);
         ejecutar->Parameters->AddWithValue("@usuario", UserName);
         ejecutar->Parameters->AddWithValue("@pass", UserPass);
-<<<<<<< HEAD
-=======
-        String^ enviardatos = "INSERT INTO usuario values (@nombre,@apellido,@usuario,@pass)";
-        MySqlCommand^ ejecutar = gcnew MySqlCommand(enviardatos, conecta);
-        ejecutar->Parameters->AddWithValue("@nombre",nombre);
-        ejecutar->Parameters->AddWithValue("@apellido",apellido);
-        ejecutar->Parameters->AddWithValue("@usuario",UserName);
-        ejecutar->Parameters->AddWithValue("@pass",UserPass);
->>>>>>> c867218 (Conexion remota Lista)
-=======
->>>>>>> 9b56156 (Cambios en las clases Consultar.h, registro.h y acceso.h, validación de usuario existente, credenciales existentes listo.)
 
         try {
             conecta->Open();
