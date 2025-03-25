360902b6cd8599a287ee9bbb7fc7df26b1dd483a
Leonardo Coronel <leonardo.coronel04@outlook.com>
2025-03-25 11:25:27 -0600

    Conexion remota Lista

--- App_CD/Acceso.cpp
+++ App_CD/Acceso.cpp
@@ -9,11 +9,8 @@ int main() {
     /*Application::EnableVisualStyles();
     Application::Run(gcnew Acceso());*/ //Instancia al formulario "Acceso.h"
     Application::EnableVisualStyles();
-<<<<<<< HEAD
-=======
     Application::Run(gcnew Acceso()); //Instancia al formulario "Acceso.h"
     Application::EnableVisualStyles();
->>>>>>> 9b56156 (Cambios en las clases Consultar.h, registro.h y acceso.h, validación de usuario existente, credenciales existentes listo.)
     Application::Run(gcnew AppUsuario()); //Instancia al formulario "AppUsuario.h"
     /*Colocar una instancia a la vez respecto al formulario inicial*/
 }
